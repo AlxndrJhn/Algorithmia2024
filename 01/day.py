9103f8b342cd0d998a5fd048b91477cbc6ca0d9b
Alexander Jahn <jahn.alexander@gmail.com>
2024-12-04 22:08:03 +0100

    day 1 part 2

--- 01/day.py
+++ 01/day.py
@@ -2,6 +2,7 @@ import itertools
 import os
 import re
 from collections import defaultdict
+from unittest import result
 
 this_folder = "\\".join(__file__.split("\\")[:-1])
 
@@ -10,17 +11,28 @@ def main(filename):
     input_data = open(f"{this_folder}/{filename}", "r").read()
 
     # Part 1
-    ENEMY_TO_POTIONS = {"A": 0, "B": 1, "C": 3}
+    ENEMY_TO_POTIONS = {"A": 0, "B": 1, "C": 3, "x": 0, "D": 5}
     result1 = sum(ENEMY_TO_POTIONS[enemy] for enemy in input_data)
     print(f"Part 1 {filename}: ", result1)
 
     # Part 2
-    result2 = 24
+    result2 = 0
+    # process in chunks of two characters at a time
+    for chunk in [input_data[i : i + 2] for i in range(0, len(input_data), 2)]:
+        regular_sum = sum(ENEMY_TO_POTIONS[enemy] for enemy in chunk)
+        if "x" in chunk:
+            result2 += regular_sum
+        else:
+            result2 += regular_sum + 2
     print(f"Part 2 {filename}: ", result2)
     return result1, result2
 
 
 if __name__ == "__main__":
-    assert main("input_example.txt") == (5, 24)
-    assert main("input.txt") == (1277, 24)
-    # assert main("input2.txt") == (42, 24)
+    try:
+        assert main("input_example.txt") == (5, 11)
+        assert main("input_example2.txt") == (22, 28)
+        assert main("input.txt") == (1277, 2277)
+        assert main("input2.txt") == (4053, 5653)
+    except AssertionError:
+        print("wrong ❌")
